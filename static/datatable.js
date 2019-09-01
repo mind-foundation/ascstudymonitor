1e1c4831f5e935b4b30243690a0ccec5da806f2c
Felix Peppert <felix.peppert@gmail.com>
2019-09-01 08:51:16 +0200

    Fully clickable entry chevron

--- static/datatable.js
+++ static/datatable.js
@@ -1,13 +1,17 @@
-const HTML_CHEVRON_RIGHT =
-  '<svg xmlns="http://www.w3.org/2000/svg" width="35.912" height="20.077" viewBox="0 0 35.912 20.077" style="transform:rotate(0);"><defs><style>.a{fill:none;stroke:#333;stroke-width:3px;}</style></defs><path class="a" d="M36.107,2581,53,2597.9l-16.9,16.9" transform="translate(2615.851 -35.046) rotate(90)"/></svg>'
-
-
 class Datatable {
   getChevronToggleHTMLString(id) {
     const $div = $(
-      `<div onClick="App.Datatable.toggle('${id}')" class='entry__chevron-wrapper'>&nbsp;</div>`
+      `<div class='entry__chevron-wrapper'>&nbsp;</div>`
     )
-    $div.append(HTML_CHEVRON_RIGHT)
+
+    const html_chevron = `
+      <svg xmlns="http://www.w3.org/2000/svg" class='toggles_datatable' data-id='${id}' width="35.912" height="20.077" viewBox="0 0 35.912 20.077" style="transform:rotate(-90deg);">
+      <defs><style>.a{fill:none;stroke:#333;stroke-width:3px;}</style></defs>
+      <path class="a" d="M36.107,2581,53,2597.9l-16.9,16.9" transform="translate(2615.851 -35.046) rotate(90)"/>
+      </svg>
+    `
+    
+    $div.append(html_chevron)
     return $div[0].outerHTML
   }
 
@@ -21,7 +25,7 @@ class Datatable {
         .closest('tr')
         .find('.entry__chevron-wrapper svg')
         .css({
-          transform: 'rotate(0deg)',
+          transform: 'rotate(-90deg)',
         })
 
         $entry
@@ -32,7 +36,7 @@ class Datatable {
         .closest('tr')
         .find('.entry__chevron-wrapper svg')
         .css({
-          transform: 'rotate(-90deg)',
+          transform: 'rotate(0deg)',
         })
 
         $entry
@@ -126,6 +130,22 @@ class Datatable {
             .trim()
         )
         .draw()
+    })
+
+    $table.on('click',  function(event) {
+      const target = event.target
+      const nodeName = target.nodeName.toLowerCase()
+
+      if (nodeName == 'div' || nodeName == 'svg' || nodeName == 'td' || nodeName == 'path') {
+        const $target = $(target)
+        const $chevron = $target.closest('div').find('svg')
+
+        if ($chevron.hasClass('toggles_datatable')) {
+          const id = $chevron.attr('data-id')
+          console.log($chevron)
+          App.Datatable.toggle(id)
+        }
+      }
     })
 
     setTimeout(() => {
