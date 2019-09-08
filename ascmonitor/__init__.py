8536ca40eb2015add2d9057fac793e1722542b36
Felix Peppert <felix.peppert@gmail.com>
2019-09-08 22:52:25 +0200

    Add sentry to backend

--- ascmonitor/__init__.py
+++ ascmonitor/__init__.py
@@ -1,2 +1,6 @@
 """ ASC Study Monitor Infrastructure """
-__version__ = "0.1.0"
+__version__ = "1.0.0"
+
+import sentry_sdk
+
+sentry_sdk.init("https://91d6a27d4de14deba93bac991e05185f@sentry.io/1661534")
