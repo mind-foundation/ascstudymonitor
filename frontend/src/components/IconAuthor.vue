c1d5298358cdfa4a30b974271807075d9e16a627
Torsten Rü <torsten@rueter.net>
2020-05-31 01:06:43 +0200

    add routing, simple list and detail views

---
+++ frontend/src/components/IconAuthor.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'icon-author',
+}
+</script>
+<template>
+  <div class="entry__icon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16.586"
+      height="20.954"
+      viewBox="0 0 16.586 20.954"
+    >
+      <g transform="translate(-107.915 -182)">
+        <path
+          class="a3"
+          d="M106.969,1409.148s.547-7.886,7.526-7.886,8.066,7.886,8.066,7.886"
+          transform="translate(1.445 -1206.262)"
+        />
+        <g class="a3" transform="translate(110 182)">
+          <circle class="a3" cx="6" cy="6" r="5.5" />
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.entry__icon {
+  width: 35px;
+  min-width: 35px;
+  display: inline-block;
+}
+
+.entry__icon .l {
+  fill: none;
+  stroke: #333;
+  stroke-width: 1px;
+}
+</style>
