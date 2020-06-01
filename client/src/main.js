806f8c16819b3ef9f6ec64c202121f886aecce36
Torsten Rü <torsten@rueter.net>
2020-06-02 01:37:24 +0200

    added sentry to vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import { Vue as VueIntegration } from '@sentry/integrations'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { sync } from 'vuex-router-sync'
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://91d6a27d4de14deba93bac991e05185f@sentry.io/1661534',
+    integrations: [
+      new VueIntegration({ Vue, attachProps: true, logErrors: true }),
+    ],
+  })
+}
 
 Vue.config.productionTip = false
 
