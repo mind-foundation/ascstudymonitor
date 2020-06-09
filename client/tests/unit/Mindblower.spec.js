0d48986b01fe5478fda20e765cfffb25cb2216f4
Torsten Rü <torsten@rueter.net>
2020-06-10 01:05:23 +0200

    fix inconsistent capitalization because ubuntu rightfully complains

--- client/tests/unit/Mindblower.spec.js
+++ client/tests/unit/Mindblower.spec.js
@@ -1,14 +1,14 @@
 import { shallowMount } from '@vue/test-utils'
-import MindBlower from '@/components/MindBlower.vue'
+import Mindblower from '@/components/Mindblower.vue'
 
 describe('Mindblower.vue', () => {
   it('matches snapshot', () => {
-    const wrapper = shallowMount(MindBlower, {})
+    const wrapper = shallowMount(Mindblower, {})
     expect(wrapper.element).toMatchSnapshot()
   })
 
   it('renders the container', () => {
-    const wrapper = shallowMount(MindBlower, {})
+    const wrapper = shallowMount(Mindblower, {})
     expect(wrapper.find('#mindblower').exists()).toBe(true)
   })
 })
