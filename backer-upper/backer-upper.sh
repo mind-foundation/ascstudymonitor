dc3b301aad016285e2690dbb98e004a30229b0f2
Felix Peppert <felix.peppert@pm.me>
2020-06-11 19:12:32 +0200

    Conjure backup solution

---
+++ backer-upper/backer-upper.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# prepare backup directory
+timestamp=$(date -u +'%Y-%m-%dT%H:%M:%S%Z')
+BACKUP_DIR="/backups/asc-studymonitor-$timestamp"
+mkdir -p $BACKUP_DIR
+cd $BACKUP_DIR
+
+# load secrets
+SECRETS_JSON="/run/secrets/asc-secret"
+source <(jq -r 'to_entries[] | "export \(.key)=\"\(.value)\""' "$SECRETS_JSON")
+
+# mongodump
+MONGO_HOST="mongo"
+MONGO_PORT="27017"
+MONGO_DATABASE="asc"
+MONGO_OUTDIR="${BACKUP_DIR}/mongodb"
+if [ -z "$BACKER_UPPER_DEV" ]; then
+    MONGO_USERNAME="root"
+    MONGO_PASSWORD="integration"
+    mongodump --host=$MONGO_HOST \
+              --port=$MONGO_PORT \
+              --username=$MONGO_USERNAME \
+              --password=$MONGO_PASSWORD \
+              --db=$MONGO_DATABASE \
+              --out="$MONGO_OUTDIR"
+else
+    mongodump --host=$MONGO_HOST \
+              --port=$MONGO_PORT \
+              --db=$MONGO_DATABASE \
+              --out="$MONGO_OUTDIR"
+fi
+
+# letsencrypt
+rsync -av /letsencrypt "$BACKUP_DIR"
+
+# prepare backup archive for upload
+cd ..
+BACKUP_ARCHIVE="asc-studymonitor-${timestamp}.tar.xz"
+tar --force-local -cJf "$BACKUP_ARCHIVE" "$BACKUP_DIR"
+
+# upload to aws
+VAULT_NAME="asc-studymonitor-backup"
+aws glacier upload-archive \
+    --account-id - \
+    --vault-name "$VAULT_NAME" \
+    --archive-description "$timestamp" \
+    --body "$BACKUP_ARCHIVE"
