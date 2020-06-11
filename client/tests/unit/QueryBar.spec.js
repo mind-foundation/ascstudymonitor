c546d0732e3894455468ccf99ac44b4a1ebaa5d2
Felix Peppert <felix.peppert@pm.me>
2020-06-11 02:55:11 +0200

    Fix client tests

--- client/tests/unit/QueryBar.spec.js
+++ client/tests/unit/QueryBar.spec.js
@@ -1,14 +1,30 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import QueryBar from '@/components/QueryBar.vue'
 
-describe('QueryBar.vue', () => {
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('QuerBar.vue', () => {
+  let store
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {
+        route: {
+          query: {},
+        },
+      },
+    })
+  })
+
   it('matches snapshot', () => {
-    const wrapper = shallowMount(QueryBar, {})
+    const wrapper = shallowMount(QueryBar, { store, localVue })
     expect(wrapper.element).toMatchSnapshot()
   })
 
   it('renders the bar', () => {
-    const wrapper = shallowMount(QueryBar, {})
+    const wrapper = shallowMount(QueryBar, { store, localVue })
     expect(wrapper.find('.top-bar').exists()).toBe(true)
   })
 })
