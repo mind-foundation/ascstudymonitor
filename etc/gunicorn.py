806c71e64e94e195c64458396e255901d6e25806
Felix Peppert <felix.peppert@pm.me>
2020-06-19 20:43:01 +0200

    Show client ip in access log

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -2,5 +2,6 @@ workers = 4
 bind = "0.0.0.0:8000"
 accesslog = "-"
 errorlog = "-"
+access_log_format = "%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s “%(r)s” %(s)s %(b)s “%(f)s” “%(a)s”"
 timeout = 120
 log_level = "info"
