d698f9d7e2bda3ca00eb6a60e37bb97c84bc387c
Torsten Rüter <torsten@rueter.net>
2020-09-28 01:05:30 +0200

    remove legacy files

--- client/src/components/Archive/__tests__/Mindblower.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import Mindblower from '@/components/Mindblower.vue'
-
-describe('Mindblower.vue', () => {
-  it('renders the container', () => {
-    const wrapper = shallowMount(Mindblower, {})
-    expect(wrapper.find('#mindblower').exists()).toBe(true)
-  })
-})
